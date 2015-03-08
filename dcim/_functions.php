a8ad11a0627a537d1a958bb3640d732c3ddbf90d
Doug <dougie+github@freshprogramming.com>
2015-03-08 07:53:25 -0700

    Fixed relative path to switch images

--- dcim/_functions.php
+++ dcim/_functions.php
@@ -3453,7 +3453,7 @@ DROP TEMPORARY TABLE IF EXISTS tmptable_1;
             	//dynamic
             	$switchWidth = 948;
             	$switchHeight = 97;
-            	$switchImage = "/images/ex4200_front.jpg";
+            	$switchImage = "../images/ex4200_front.jpg";
             	$topOffset = 25;
             	$bottomOffset = 30;
             	$set1Offset = 18;
@@ -3467,7 +3467,7 @@ DROP TEMPORARY TABLE IF EXISTS tmptable_1;
             	{
                 	$switchWidth = 950;
                 	$switchHeight = 91;
-                	$switchImage = "/images/patchpanel.jpg";
+                	$switchImage = "../images/patchpanel.jpg";
                 	$topOffset = 0;
                 	$bottomOffset = 38;
                 	$set1Offset = 60;
@@ -3484,11 +3484,11 @@ DROP TEMPORARY TABLE IF EXISTS tmptable_1;
             	}
             	else if($deviceInfo->name=="EX3200 24p" || $deviceInfo->name=="EX4200 24p")
             	{
-            	    $switchImage = "/images/ex4200_24p_front.jpg";
+            	    $switchImage = "../images/ex4200_24p_front.jpg";
             	}
             	else if($deviceInfo->name=="WS-X6348")
             	{
-            	    $switchImage = "/images/ws-x6348_front.jpg";
+            	    $switchImage = "../images/ws-x6348_front.jpg";
                 	$switchWidth = 950;
                 	$switchHeight = 105;
                 	$topOffset = 25;
@@ -3501,7 +3501,7 @@ DROP TEMPORARY TABLE IF EXISTS tmptable_1;
             	}
             	else if($deviceInfo->name=="Catalyst 3550")
             	{
-            	    $switchImage = "/images/catalyst2950_front.jpg";
+            	    $switchImage = "../images/catalyst2950_front.jpg";
                 	$switchWidth = 950;
                 	$switchHeight = 89;
                 	$topOffset = 19;
