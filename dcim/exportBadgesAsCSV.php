6c18ee3b3cc439e99729f488f1af7893ef327e5e
Doug <dougie+github@freshprogramming.com>
2015-03-11 03:37:56 -0700

    Added location access list to badge export

--- dcim/exportBadgesAsCSV.php
+++ dcim/exportBadgesAsCSV.php
@@ -25,11 +25,18 @@
 		$result = array();
 		
 		//select empty as customer to keep return results to match search query
-		$query = "SELECT c.hno, c.name AS customer, b.name, b.badgeno, b.issue, b.hand
-			FROM dcim_badge AS b
-			LEFT JOIN dcim_customer AS c ON c.hno = b.hno
-			WHERE b.status =  'A'
-			ORDER BY c.name, b.name";
+		$query = "SELECT cur.hno, cur.cno, cur.name AS customer, cur.name, cur.badgeno, cur.issue, cur.hand, GROUP_CONCAT(cur.fullloc SEPARATOR ', ') AS alllocs
+						FROM (SELECT b.badgeid, c.hno, c.cno, c.name AS customer, b.name, b.badgeno, b.issue, b.hand,
+							CONCAT(s.name,' CA',CAST(l.colo AS UNSIGNED),' ',l.name) AS fullloc
+							FROM dcim_badge AS b
+								LEFT JOIN dcim_customer AS c ON c.hno = b.hno
+								LEFT JOIN dcim_device AS d ON d.hno=c.hno
+								LEFT JOIN dcim_location AS l ON d.locationid=l.locationid 
+								LEFT JOIN dcim_site AS s ON s.siteid=l.siteid 
+							WHERE b.status =  'A' AND d.type IN ('C','F','H') AND d.status='A') 
+								AS cur
+						GROUP BY cur.badgeid
+						ORDER BY cur.customer, cur.name";
 
 		if (!($stmt = $mysqli->prepare($query)))
 		{
@@ -40,7 +47,7 @@
 			
 		$stmt->execute();
 		$stmt->store_result();
-		$stmt->bind_result($hNo, $customer,$name, $badgeNo, $issue, $hand);
+		$stmt->bind_result($hNo, $cNo, $customer,$name, $badgeNo, $issue, $hand, $locs);
 		$count = $stmt->num_rows;
 	
 		if($count>0)
@@ -48,12 +55,12 @@
 			//create result array
 			//headers
 			$result[] = array("Active Badge list as of ".date("F j, Y"));//EX: March 10, 2001
-			$result[] = array("HNo", "Customer Name","Badge Holder", "Badge No", "Issue Date", "Enrol date");
+			$result[] = array("HNo", "CNo", "Customer Name","Badge Holder", "Badge No", "Issue Date", "Enrol date","Access");
 			
 			//list result data
 			while ($stmt->fetch()) 
 			{
-				$result[] = array($hNo, $customer,$name, $badgeNo, $issue, $hand);
+				$result[] = array($hNo, $cNo, $customer,$name, $badgeNo, $issue, $hand, $locs);
 			}
 		}
 		return $result;
