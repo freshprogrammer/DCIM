5497d5f6a6cec928088a71786f3719136752a2fc
Doug <dougie+github@freshprogramming.com>
2015-03-25 04:56:00 -0700

    Added Power Readings Export to CSV with custom CSV formmatting options

--- dcim/createReport.php
+++ dcim/createReport.php
@@ -12,13 +12,9 @@
 	$report = GetInput("report");
 	
 	if($report==="ActiveBadgeList")
-	{
 		OutputCSV("badgelist.csv",CreateBadgeExportArray());
-	}
-	else if($report==="OtherReport")
-	{
-		OutputCSV("badgelist.csv",CreateBadgeExportArray());
-	}
+	else if($report==="PowerAudit")
+		OutputCSV("PowerAudit-".date("Y-m-d").".csv",CreatePowerAuditExportArray());
 	else 
 	{
 		echo "No Report Specified";
@@ -33,21 +29,20 @@
 		
 		//select empty as customer to keep return results to match search query
 		$query = "SELECT cur.hno, cur.cno, cur.customer, cur.name, cur.badgeno, cur.issue, cur.hand, GROUP_CONCAT(cur.fullloc SEPARATOR ', ') AS alllocs
-						FROM (SELECT b.badgeid, c.hno, c.cno, c.name AS customer, b.name, b.badgeno, b.issue, b.hand,
-							CONCAT(s.name,' CA',CAST(l.colo AS UNSIGNED),' ',l.name) AS fullloc
-							FROM dcim_badge AS b
-								LEFT JOIN dcim_customer AS c ON c.hno = b.hno
-								LEFT JOIN dcim_device AS d ON d.hno=c.hno
-								LEFT JOIN dcim_location AS l ON d.locationid=l.locationid 
-								LEFT JOIN dcim_site AS s ON s.siteid=l.siteid 
-							WHERE b.status =  'A' AND d.type IN ('C','F','H') AND d.status='A') 
-								AS cur
-						GROUP BY cur.badgeid
-						ORDER BY cur.customer, cur.name";
+			FROM (SELECT b.badgeid, c.hno, c.cno, c.name AS customer, b.name, b.badgeno, b.issue, b.hand,
+				CONCAT(s.name,' CA',CAST(l.colo AS UNSIGNED),' ',l.name) AS fullloc
+				FROM dcim_badge AS b
+					LEFT JOIN dcim_customer AS c ON c.hno = b.hno
+					LEFT JOIN dcim_device AS d ON d.hno=c.hno
+					LEFT JOIN dcim_location AS l ON d.locationid=l.locationid 
+					LEFT JOIN dcim_site AS s ON s.siteid=l.siteid 
+				WHERE b.status =  'A' AND d.type IN ('C','F','H') AND d.status='A') 
+					AS cur
+			GROUP BY cur.badgeid
+			ORDER BY cur.customer, cur.name";
 
 		if (!($stmt = $mysqli->prepare($query)))
 		{
-			//TODO handle errors better
 			$result[] = array("Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error . "<BR>");
 			return $result;
 		}
@@ -73,4 +68,70 @@
 		return $result;
 	}
 	
+	function CreatePowerAuditExportArray()
+	{
+		global $mysqli;
+		
+		$result = array();
+		$siteID = 0;
+		
+		$query = "SELECT s.name AS site,l.colo,l.name AS location,p.panel,p.circuit,c.name AS cust, c.hno, p.cload, (p.cload/p.amps*100) AS percent,p.amps, p.volts, p.status, p.editdate
+			FROM dcim_location AS l
+				INNER JOIN dcim_powerloc AS pl ON l.locationid=pl.locationid
+				INNER JOIN dcim_power AS p ON pl.powerid=p.powerid
+				INNER JOIN dcim_site AS s ON l.siteid=s.siteid
+				LEFT JOIN dcim_device AS d ON l.locationid=d.locationid
+				LEFT JOIN dcim_customer AS c ON d.hno=c.hno
+			WHERE l.siteid=$siteID
+			GROUP BY s.siteid,l.colo,l.name,p.panel,p.circuit
+			ORDER BY l.colo,l.name,p.panel,CAST(p.circuit AS UNSIGNED)";
+
+		if (!($stmt = $mysqli->prepare($query)))
+		{
+			$result[] = array("Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error . "<BR>");
+			return $result;
+		}
+			
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($site,$colo,$locationName,$panel,$circuit,$cust,$hNo,$cLoad,$percent,$amps,$volts,$status,$editDate);
+		$count = $stmt->num_rows;
+		
+		if($count>0)
+		{
+			//create result array
+			//headers
+			$result[] = array("All Power Readings as of ".date("F j, Y"));//EX: March 10, 2001
+			$result[] = array("Customer","H#","Location","Panel","Circuit","Reading","Reading%","Amps","Volts","On/Off","Date");
+
+			$showNAForEmptyLocations = true;
+			$skipLinesBetweenLocationsAndOnlyFirstCustName = true;
+			$lastLoc = "";
+			//list result data
+			while ($stmt->fetch())
+			{
+				$fullLocationName = FormatLocation($site, $colo, $locationName);
+				if($showNAForEmptyLocations)
+					$cust = (strlen($cust)>0)?$cust:"N/A";
+				$panel = (strrpos($panel, '-')||strrpos($panel, '/'))?("=\"$panel\""):$panel;
+				$circuit = ($volts==208)?("=\"".$circuit."/".($circuit+2)."\""):$circuit;
+				if($skipLinesBetweenLocationsAndOnlyFirstCustName)
+				{
+					if($lastLoc==$fullLocationName)
+					{
+						$cust = "";//dont show this 
+						$hNo = "";
+					}
+					else 
+					{
+						if(strlen($lastLoc)>0)//this is just for the first loc
+							$result[] = array("");
+					}
+					$lastLoc = $fullLocationName;
+				}
+				$result[] = array($cust,$hNo,$fullLocationName,$panel,$circuit,$cLoad."A",substr($percent,0,5)."%",$amps."A",$volts."V",($status==="A")?"On":"Off",substr($editDate,0,10));
+			}
+		}
+		return $result;
+	}
 ?>
