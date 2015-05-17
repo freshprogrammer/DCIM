128c4fd117bdf9589d641111c5e82de3c307b2a7
Doug <dougie+github@freshprogramming.com>
2015-05-17 05:15:54 -0700

    Moved search placeholders to custom functions

--- index.php
+++ index.php
@@ -176,85 +176,11 @@
 	//search
 	if(UserHasReadPermission())
 	{
-		$placeholder = "";
-		//random placeholders
-		if(true)
-		{
-			$linesOfCode = CountLinesInDir();
-			$dbRecs = CountDBRecords();
-			$rand = rand(0,90);
-			$searchPlaceHolders = array();
-			//ROUGH LIMIT           "------------------------"
-			$searchPlaceHolders[] = "Search";
-			$searchPlaceHolders[] = "Search from here";
-			$searchPlaceHolders[] = "Everyone loves to search";
-			$searchPlaceHolders[] = "Your companion";
-			$searchPlaceHolders[] = "Like a pet but useful";
-			$searchPlaceHolders[] = "Type here";
-			$searchPlaceHolders[] = "Waiting...";
-			$searchPlaceHolders[] = "Got your back";
-			$searchPlaceHolders[] = "It's cool, I got this";
-			$searchPlaceHolders[] = "You know what to do";
-			$searchPlaceHolders[] = "Self explanatory";
-			$searchPlaceHolders[] = "This is where your search";
-			$searchPlaceHolders[] = "See KB for details";
-			$searchPlaceHolders[] = "Search is your friend";
-			$searchPlaceHolders[] = "Search could save your life some day";
-			$searchPlaceHolders[] = "Indexes to the rescue";
-			$searchPlaceHolders[] = "Pioneering Omni-Search";
-			$searchPlaceHolders[] = "Into the depths";
-			$searchPlaceHolders[] = "Almost lunch time";
-			$searchPlaceHolders[] = "Who's behind you";
-			$searchPlaceHolders[] = "Not on break";
-			$searchPlaceHolders[] = "Almost never on break";
-			$searchPlaceHolders[] = "Like an unpaid intern";
-			$searchPlaceHolders[] = "I am so smart... SMRT";
-			$searchPlaceHolders[] = "Your coffee is ready";
-			$searchPlaceHolders[] = "Search never sleeps";
-			$searchPlaceHolders[] = "Wont save you on car inssurance";
-			$searchPlaceHolders[] = "Smoke free for $rand days";
-			$searchPlaceHolders[] = "NSA free for 0 days";
-			$searchPlaceHolders[] = "Not a calculator";
-			$searchPlaceHolders[] = "Still not a calculator";
-			$searchPlaceHolders[] = "Sponsored by your tax dollars";
-			$searchPlaceHolders[] = "You are here *";
-			$searchPlaceHolders[] = "I search to serve";
-			$searchPlaceHolders[] = "Give a man a match...";
-			$searchPlaceHolders[] = "Not from The Simpsons&reg;";
-			$searchPlaceHolders[] = "One man's dream of data";
-			$searchPlaceHolders[] = "Please like and subscribe";
-			$searchPlaceHolders[] = "Doesn't search Facebook";
-			$searchPlaceHolders[] = "No Facebook login";
-			$searchPlaceHolders[] = "No like buttons here";
-			
-			$searchPlaceHolders[] = "$linesOfCode+ lines of code";
-			$searchPlaceHolders[] = "$linesOfCode+ free range lines";
-			$searchPlaceHolders[] = "$linesOfCode lines, but cutting back";
-			$searchPlaceHolders[] = "10K+ line club";
-			$searchPlaceHolders[] = "$dbRecs+ DB Records";
-			$searchPlaceHolders[] = "$dbRecs Records and counting";
-			
-			$searchPlaceHolders[] = "Google's nemesis";
-			$searchPlaceHolders[] = "Googol * Googol";
-			$searchPlaceHolders[] = "Who cares about Bing";
-			$searchPlaceHolders[] = "Remember Yahoo!?";
-			$searchPlaceHolders[] = "DuckDuckGo";
-			$searchPlaceHolders[] = "One DogPile for all";
-			$searchPlaceHolders[] = "Making search Cuil again";
-			$searchPlaceHolders[] = "Not AltaVista";
-			$searchPlaceHolders[] = "Fetch Jeeves";
-			$searchPlaceHolders[] = "Fresh Search";
-			
-			$searchPlaceHolders[] = "e.g. H######";
-			$searchPlaceHolders[] = "e.g. C######";
-			$searchPlaceHolders[] = "e.g. Company name";
-			$searchPlaceHolders[] = "e.g. Company note";
-			$searchPlaceHolders[] = "e.g. Device Name";
-			$searchPlaceHolders[] = "e.g. Badge Holder";
-			$searchPlaceHolders[] = "e.g. Location Name";
-			$placeholder = $searchPlaceHolders[array_rand($searchPlaceHolders)];
-		}
-
+		$placeholder = "Search";
+		
+		if(true)//custom search placeholder(s)
+			$placeholder = CustomFunctions::GetSearchPlaceholder();
+		
 		$menuItems .= "<td class='dr-toolbar-int rich-toolbar-item'  align='right''>\n";
 		$menuItems .= "	<form name='MainSearch' action='./' method=get>\n";
 		$menuItems .= "		Search: <input type='text' name='search' placeholder=\"$placeholder\">\n";
--- lib/DCIMCustomFunctions.php
+++ lib/DCIMCustomFunctions.php
@@ -25,5 +25,83 @@
 			$result .= "<a class='navLinks' href='?roomid=6'>CA5</a>&nbsp;\n";
 			return $result;
 		}
+		
+		public static function GetSearchPlaceholder()
+		{
+			$linesOfCode = CountLinesInDir();
+			$dbRecs = CountDBRecords();
+			$rand = rand(0,90);
+			$searchPlaceHolders = array();
+			//ROUGH LIMIT           "------------------------"
+			$searchPlaceHolders[] = "Search";
+			$searchPlaceHolders[] = "Search from here";
+			$searchPlaceHolders[] = "Everyone loves to search";
+			$searchPlaceHolders[] = "Your companion";
+			$searchPlaceHolders[] = "Like a pet but useful";
+			$searchPlaceHolders[] = "Type here";
+			$searchPlaceHolders[] = "Waiting...";
+			$searchPlaceHolders[] = "Got your back";
+			$searchPlaceHolders[] = "It's cool, I got this";
+			$searchPlaceHolders[] = "You know what to do";
+			$searchPlaceHolders[] = "Self explanatory";
+			$searchPlaceHolders[] = "This is where your search";
+			$searchPlaceHolders[] = "See KB for details";
+			$searchPlaceHolders[] = "Search is your friend";
+			$searchPlaceHolders[] = "Search could save your life some day";
+			$searchPlaceHolders[] = "Indexes to the rescue";
+			$searchPlaceHolders[] = "Pioneering Omni-Search";
+			$searchPlaceHolders[] = "Into the depths";
+			$searchPlaceHolders[] = "Almost lunch time";
+			$searchPlaceHolders[] = "Who's behind you";
+			$searchPlaceHolders[] = "Not on break";
+			$searchPlaceHolders[] = "Almost never on break";
+			$searchPlaceHolders[] = "Like an unpaid intern";
+			$searchPlaceHolders[] = "I am so smart... SMRT";
+			$searchPlaceHolders[] = "Your coffee is ready";
+			$searchPlaceHolders[] = "Search never sleeps";
+			$searchPlaceHolders[] = "Wont save you on car inssurance";
+			$searchPlaceHolders[] = "Smoke free for $rand days";
+			$searchPlaceHolders[] = "NSA free for 0 days";
+			$searchPlaceHolders[] = "Not a calculator";
+			$searchPlaceHolders[] = "Still not a calculator";
+			$searchPlaceHolders[] = "Sponsored by your tax dollars";
+			$searchPlaceHolders[] = "You are here *";
+			$searchPlaceHolders[] = "I search to serve";
+			$searchPlaceHolders[] = "Give a man a match...";
+			$searchPlaceHolders[] = "Not from The Simpsons&reg;";
+			$searchPlaceHolders[] = "One man's dream of data";
+			$searchPlaceHolders[] = "Please like and subscribe";
+			$searchPlaceHolders[] = "Doesn't search Facebook";
+			$searchPlaceHolders[] = "No Facebook login";
+			$searchPlaceHolders[] = "No like buttons here";
+			
+			$searchPlaceHolders[] = "$linesOfCode+ lines of code";
+			$searchPlaceHolders[] = "$linesOfCode+ free range lines";
+			$searchPlaceHolders[] = "$linesOfCode lines, but cutting back";
+			$searchPlaceHolders[] = "10K+ line club";
+			$searchPlaceHolders[] = "$dbRecs+ DB Records";
+			$searchPlaceHolders[] = "$dbRecs Records and counting";
+			
+			$searchPlaceHolders[] = "Google's nemesis";
+			$searchPlaceHolders[] = "Googol * Googol";
+			$searchPlaceHolders[] = "Who cares about Bing";
+			$searchPlaceHolders[] = "Remember Yahoo!?";
+			$searchPlaceHolders[] = "DuckDuckGo";
+			$searchPlaceHolders[] = "One DogPile for all";
+			$searchPlaceHolders[] = "Making search Cuil again";
+			$searchPlaceHolders[] = "Not AltaVista";
+			$searchPlaceHolders[] = "Fetch Jeeves";
+			$searchPlaceHolders[] = "Fresh Search";
+			
+			$searchPlaceHolders[] = "e.g. H######";
+			$searchPlaceHolders[] = "e.g. C######";
+			$searchPlaceHolders[] = "e.g. Company name";
+			$searchPlaceHolders[] = "e.g. Company note";
+			$searchPlaceHolders[] = "e.g. Device Name";
+			$searchPlaceHolders[] = "e.g. Badge Holder";
+			$searchPlaceHolders[] = "e.g. Location Name";
+			
+			return $searchPlaceHolders[array_rand($searchPlaceHolders)];
+		} 
 	}
 ?>
