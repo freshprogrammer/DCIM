2df11a95df85ef58337f884b472c9b63deb773cd
Doug <dougie+github@freshprogramming.com>
2015-08-26 06:07:24 -0700

    Optimized to skip creating popup text in rendering within parent

--- lib/functions.php
+++ lib/functions.php
@@ -6507,67 +6507,70 @@ DROP TEMPORARY TABLE IF EXISTS tmptable_1;
 				$firstDevice = true;
 				$popupText = "";
 				
-				//countL should always be >=1
-				if(true || $countL>1 || $countD>1)//XXX this extra SELECT per locaition* could be avoided if the counts were 1 but would require duplication the popup text generation code 
+				if(!$renderingWithinParent)
 				{
-					//full locattion name, type, size
-					//full device name type,size, sorted by cust, devicename
-					
-					//look up each location  & customer here
-					$query2 = "SELECT l.locationid, l.name, l.xpos, l.ypos, l.width, l.depth, l.orientation, s.name, r.fullname, d.hno, c.name AS cust, d.deviceid, d.name as device
-						FROM dcim_location AS l
-							LEFT JOIN dcim_device AS d ON d.locationid=l.locationid AND d.status = 'A'
-							LEFT JOIN dcim_customer AS c ON d.hno = c.hno
-							LEFT JOIN dcim_room AS r ON r.roomid = l.roomid
-							LEFT JOIN dcim_site AS s ON s.siteid = r.siteid
-						WHERE l.xpos=? AND l.ypos=? AND l.width=? AND l.depth=? AND l.orientation=?
-						ORDER BY l.name, c.name, c.hno, d.name";
-					
-					if (!($stmt2 = $mysqli->prepare($query2)) || !$stmt2->bind_Param('dddds', $xPos,$yPos,$width,$depth,$orientation) || !$stmt2->execute())
-						$errorMessage[]= "CreateRoomLayout() SQL setup 2 failed: (" . $mysqli->errno . ") " . $mysqli->error;
-					else
+					//countL should always be >=1
+					if(true || $countL>1 || $countD>1)//XXX this extra SELECT per locaition* could be avoided if the counts were 1 but would require duplication the popup text generation code 
 					{
-						$stmt2->store_result();
-						$stmt2->bind_result($locationID, $name, $xPos, $yPos, $width, $depth, $orientation, $site, $room, $hNo, $customer, $deviceid, $deviceName);
+						//full locattion name, type, size
+						//full device name type,size, sorted by cust, devicename
 						
-						$lastCustomerID = PHP_INT_MAX;
-						while($stmt2->fetch())
+						//look up each location  & customer here
+						$query2 = "SELECT l.locationid, l.name, l.xpos, l.ypos, l.width, l.depth, l.orientation, s.name, r.fullname, d.hno, c.name AS cust, d.deviceid, d.name as device
+							FROM dcim_location AS l
+								LEFT JOIN dcim_device AS d ON d.locationid=l.locationid AND d.status = 'A'
+								LEFT JOIN dcim_customer AS c ON d.hno = c.hno
+								LEFT JOIN dcim_room AS r ON r.roomid = l.roomid
+								LEFT JOIN dcim_site AS s ON s.siteid = r.siteid
+							WHERE l.xpos=? AND l.ypos=? AND l.width=? AND l.depth=? AND l.orientation=?
+							ORDER BY l.name, c.name, c.hno, d.name";
+						
+						if (!($stmt2 = $mysqli->prepare($query2)) || !$stmt2->bind_Param('dddds', $xPos,$yPos,$width,$depth,$orientation) || !$stmt2->execute())
+							$errorMessage[]= "CreateRoomLayout() SQL setup 2 failed: (" . $mysqli->errno . ") " . $mysqli->error;
+						else
 						{
-							$newLocation = ($lastLocationID!=$locationID);
-							$newCustomer = ($lastCustomerID!=$hNo);
-							$fullLocationName = FormatLocation($site, $room, $name);
+							$stmt2->store_result();
+							$stmt2->bind_result($locationID, $name, $xPos, $yPos, $width, $depth, $orientation, $site, $room, $hNo, $customer, $deviceid, $deviceName);
 							
-							if(!$firstDevice)
-								$popupText .= "<BR>";
-							
-							if($newLocation)
-								$popupText .= "<b><a href='?locationid=$locationID'>$fullLocationName</a></b><BR>\n";
-							
-							if($hNo==NULL)
-								$popupText .= "EMPTY";
-							else
+							$lastCustomerID = PHP_INT_MAX;
+							while($stmt2->fetch())
 							{
-								if($newCustomer)
-									$popupText .= "<a href='?host=$hNo'>$customer</a><BR>\n";
-								$popupText .= "<a href='?deviceid=$deviceid'>$deviceName</a>";
+								$newLocation = ($lastLocationID!=$locationID);
+								$newCustomer = ($lastCustomerID!=$hNo);
+								$fullLocationName = FormatLocation($site, $room, $name);
+								
+								if(!$firstDevice)
+									$popupText .= "<BR>";
+								
+								if($newLocation)
+									$popupText .= "<b><a href='?locationid=$locationID'>$fullLocationName</a></b><BR>\n";
+								
+								if($hNo==NULL)
+									$popupText .= "EMPTY";
+								else
+								{
+									if($newCustomer)
+										$popupText .= "<a href='?host=$hNo'>$customer</a><BR>\n";
+									$popupText .= "<a href='?deviceid=$deviceid'>$deviceName</a>";
+								}
+								$firstDevice = false;
+								$lastLocationID=$locationID;
+								$lastCustomerID = $hNo;
 							}
-							$firstDevice = false;
-							$lastLocationID=$locationID;
-							$lastCustomerID = $hNo;
 						}
 					}
+					else
+					{ /* disabled since this code is not active and not up to date - see above comment
+						$fullLocationName = FormatLocation($site, $room, $name);
+						if($countD==0)
+							$popupText = "<b><a href='?locationid=$locationID'>$fullLocationName</a></b><BR>Empty";
+						else if($countD==1)
+							$popupText = "<b><a href='?locationid=$locationID'>$fullLocationName</a></b><BR>
+						<a href='?host=$hNo'>$customer</a><BR>
+						<a href='?deviceid=$deviceid'>$deviceName</a>";*/
+					}
 				}
-				else
-				{ /* disabled since this code is not active and not up to date - see above comment
-					$fullLocationName = FormatLocation($site, $room, $name);
-					if($countD==0)
-						$popupText = "<b><a href='?locationid=$locationID'>$fullLocationName</a></b><BR>Empty";
-					else if($countD==1)
-						$popupText = "<b><a href='?locationid=$locationID'>$fullLocationName</a></b><BR>
-					<a href='?host=$hNo'>$customer</a><BR>
-					<a href='?deviceid=$deviceid'>$deviceName</a>";*/
-				}
-				$result .= CreateLocationLayout($locationID, $name, $xPos, $yPos, $width, $depth, $orientation, $countD, $hNo, $customer, $parentWidth, $parentDepth,$renderingWithinParent, $popupText);
+				$result .= CreateLocationLayout($locationID, $name, $xPos, $yPos, $width, $depth, $orientation, $countD, $hNo, $customer, $parentWidth, $parentDepth, $renderingWithinParent, $popupText);
 			}
 		}
 		
